31bf6ebe3e8e2c54f6a6cce92f3267a58dbd1044
php 01 <php.01@penadinho.cec.ime.usp.br>
2018-01-19 22:22:06 -0200

    classe conta

---
+++ Conta.php
@@ -0,0 +1,22 @@
+<?php
+
+class Conta
+{
+  public $numero; // atributos
+  public $saldo; // atributos
+
+}
+
+$x = new Conta();
+$x->numero = 123;
+$x->saldo = 1000;
+
+echo "Número da conta x: $x->numero" . PHP_EOL;
+echo "Saldo da conta x: $x->saldo" . PHP_EOL;
+
+$y = new Conta();
+$y->numero = 133;
+$y->saldo = 560;
+
+echo "Número da conta y: $y->numero" . PHP_EOL;
+echo "Saldo da conta y: $y->saldo" . PHP_EOL;
