9deeab372fd366c7ce086c77291a8bdfbf8cd4f3
php 01 <php.01@velma.cec.ime.usp.br>
2018-01-22 20:55:37 -0200

    formatando data com o carbon e testando classe cartao de credito a partir de outro arquivo

--- TestaCartaoDeCredito.php
+++ TestaCartaoDeCredito.php
@@ -1,12 +1,15 @@
 <?php
 
 require 'lib/Carbon.php';
+require 'CartaoDeCredito.php';
 
 use Carbon\Carbon;
 
 $cartao = new CartaoDeCredito();
 $cartao->numero = 55555;
-$cartao->dataDeValidade = "22/03/2022";
+$cartao->dataDeValidade =
+  Carbon::createFromDate(2022, 3, 7,
+  'America/Sao_Paulo');
 
 echo "Número: $cartao->numero" . PHP_EOL;
-echo "Validade: $cartao->dataDeValidade" . PHP_EOL;
+echo "Validade: {$cartao->dataDeValidade->format('d/m/Y')}" . PHP_EOL;
